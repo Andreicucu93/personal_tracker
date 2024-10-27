f6bed7ed3bf3141c6e61d2669ab50fc5be5b6702
unknown <Andrei_Cucu@outlook.com>
2024-10-27 17:40:00 +0200

    third update

--- about update.md
+++ about update.md
@@ -1,1 +1,3 @@
 This has 2 random prints.
+
+This 3rd update has 3 random prints.
--- main.py
+++ main.py
@@ -3,3 +3,5 @@ import random
 print(random.randint(0, 100))
 
 print(random.randint(1, 101))
+
+print(random.randing(2, 102))
